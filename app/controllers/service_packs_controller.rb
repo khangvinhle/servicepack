81e007badf651330842ee5611bcf18a8c4ed4b8c
Kiet <kietnguyen.op@gmail.com>
2019-01-08 12:15:48 +0700

    new and index action for sp

--- app/controllers/service_packs_controller.rb
+++ app/controllers/service_packs_controller.rb
@@ -1,2 +1,17 @@
 class ServicePacksController < ApplicationController
+
+	# only allow admin
+	before_action :require_admin
+
+	# Specifying Layouts for Controllers, looking at OPENPROJECT_ROOT/app/views/layouts/admin
+	layout 'admin'
+	
+	def index
+	end
+
+	def new
+	end
+
+
+
 end
