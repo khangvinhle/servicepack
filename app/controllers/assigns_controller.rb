4e4735bafc806ef2f5ab612359fe2cccf37b7eea
Le Cao Minh Tam <lcao.mtam@gmail.com>
2019-01-13 20:16:05 +0700

    Put some code in AssignsCtrller#assign
    Former-commit-id: b6c6cd27361f323fcd7a2ee6d36c05e6e7cf270b

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -4,7 +4,7 @@ class AssignsController < ApplicationController
 
 	def assign
 		if !@project.module_enabled?(:service_packs)
-			render_502 and return
+			render_400 and return
 		end
 		if @project.assigns.where(assigned: true)
 			flash[:now] = "You must unassign first!"
@@ -15,26 +15,25 @@ class AssignsController < ApplicationController
 			if assignable = !(@service_pack.assigns.where(assigned: true))
 				ActiveRecord::Base.transaction do
 					@project.assigns.update_all!(assigned: false)
-					if @assign_record = ServicePack.assigns.where(project_id: @project.id)
+					if @assign_record = ServicePack.assigns.where(project_id: @project.id) || @project.assigns.new
 						@assign_record.assigned = true
 						@assign_record.service_pack_id = params[:service_pack]
-						@assign_record.save!
-					else
-						@assign_record = @project.assigns.new
-						@assign_record.assigned = true
-						@assign_record.service_pack_id = params[:service_pack]
-						@assign_record.save!						
+						@assign_record.save!					
 					end
 				end
 				return
 			else
 				# already assigned
 				flash[:now] = "Service Pack #{@service_pack.name} has been already assigned"
-				render_502 and return
+				render_400 and return
 			end
 		end
 		flash[:now] = "Service Pack not found"
-		render_502
+		render_400
+	end
+
+	def unassign
+
 	end
 	def show
 		if !@project.module_enabled?(:service_packs)
