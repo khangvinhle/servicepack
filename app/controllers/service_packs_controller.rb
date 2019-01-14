1fa122c569bb0ab8e537de4ffb938744c8d0cf40
khangvinhle <1559019@itec.hcmus.edu.vn>
2019-01-14 17:07:36 +0700

    develop: save a service pack
    Former-commit-id: b717f65b05c19c48967b12d9e231ee7e31065dc9

--- app/controllers/service_packs_controller.rb
+++ app/controllers/service_packs_controller.rb
@@ -1,39 +1,36 @@
 class ServicePacksController < ApplicationController
+  # only allow admin
+  before_action :require_admin
 
-	# only allow admin
-	before_action :require_admin
+  # Specifying Layouts for Controllers, looking at OPENPROJECT_ROOT/app/views/layouts/admin
+  layout 'admin'
 
-	# Specifying Layouts for Controllers, looking at OPENPROJECT_ROOT/app/views/layouts/admin
-	layout 'admin'
-	
-	def index
-		@service_packs = ServicePack.all 
-	end
+  def index
+    @service_packs = ServicePack.all
+  end
 
-	def new
-		@service_pack = ServicePack.new
-		TimeEntryActivity.count.times { @service_pack.mapping_rates.build }
-	end
+  def new
+    @service_pack = ServicePack.new
+    TimeEntryActivity.count.times {@service_pack.mapping_rates.build}
+  end
 
-	def show
-		@service_pack = ServicePack.find(params[:id])
-	end
+  def show
+    @service_pack = ServicePack.find(params[:id])
+  end
 
-	def create
-		@service_pack = ServicePack.new(service_pack_params)
+  def create
+    @service_pack = ServicePack.new(service_pack_params)
+    if @service_pack.save
+      # redirect_to @service_pack
+    else
+      render 'new'
+    end
+  end
 
-		@service_pack.default_remained_units
+  private
 
-		if @service_pack.save
-			redirect_to @service_pack
-		else
-			render 'new'
-		end
-	end
-
-	private 
-		def service_pack_params
-			params.require(:service_pack).permit(:name, :total_units, :started_date, :expired_date, :threshold1, :threshold2)
-		end
+  def service_pack_params
+    params.require(:service_pack).permit(:name, :total_units, :started_date, :expired_date, :threshold1, :threshold2, mapping_rates_attributes: [:id, :activity_id, :service_pack_id, :units_per_hour])
+  end
 
 end
