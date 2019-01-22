b09373610e74a436088b25e963c42afc7f41ef4d
Le Cao Minh Tam <lcao.mtam@gmail.com>
2019-01-22 09:35:47 +0700

    Setting ground for statistics function
    Former-commit-id: 01efb459c09eb5da2b68c020222f52c0627fe84d

---
+++ app/helpers/service_pack_statistics.rb
@@ -0,0 +1,13 @@
+class ServicePackStatistics
+	# waiting for schema
+	# acceptance day: 80h left
+	def analyze_by_activity(service_pack=nil)
+		q = <<-SQL
+
+			SQL
+	end
+
+	def query_executor(service_pack=nil, begin_period=nil, begin_post_period=nil)
+
+	end
+end
