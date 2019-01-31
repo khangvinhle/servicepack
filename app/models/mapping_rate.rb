6dde2bca6fc34fc76a9800a746a9f082145e3d97
Le Cao Minh Tam <lcao.mtam@gmail.com>
2019-01-31 17:09:35 +0700

    Revert most of commit #74637f5b

--- app/models/mapping_rate.rb
+++ app/models/mapping_rate.rb
@@ -1,7 +1,6 @@
 class MappingRate < ApplicationRecord
   belongs_to :activity, class_name: 'TimeEntryActivity', foreign_key: :activity_id
-  belongs_to :service_pack, dependent: :destroy
-
+  belongs_to :service_pack
   # validates_uniqueness_of :activity, scope: :service_pack
 
   validates_numericality_of :units_per_hour, only_integer: true, greater_than_or_equal_to: 0
--- app/models/service_pack_entry.rb
+++ app/models/service_pack_entry.rb
@@ -1,4 +1,5 @@
 class ServicePackEntry < ApplicationRecord
-	belongs_to :time_entry, dependent: :destroy
+	belongs_to :time_entry
+	# dependent is for THIS association.
 	belongs_to :service_pack, inverse_of: :service_pack_entries
 end
