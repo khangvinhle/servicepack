769c81214354bf13545456414fd2797dd96c5e48
khangvinhle <1559019@itec.hcmus.edu.vn>
2019-02-13 11:21:54 +0700

    reformat code

--- app/mailers/service_packs_mailer.rb
+++ app/mailers/service_packs_mailer.rb
@@ -1,22 +1,24 @@
 class ServicePacksMailer < ApplicationMailer
   default from: 'op@example.com'
+
   def expired_email(user, service_pack)
     @user = user
     @sp = service_pack
     # binding.pry
 
     mail to: @user.mail, subject: "The service pack #{@sp.name} has expired" do |format|
-    	format.text
-    	format.html
+      format.text
+      format.html
     end
   end
+
   def notify_under_threshold1(user, service_pack)
-  	@user = user
-  	@sp = service_pack
+    @user = user
+    @sp = service_pack
     # binding.pry
     mail to: @user.mail, subject: "The service pack #{@sp.name} is running out" do |format|
-    	format.text
-    	format.html
+      format.text
+      format.html
     end
   end
 end
