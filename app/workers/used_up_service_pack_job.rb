b161651a0784a511e39663432c08ffe886cb0b3b
khangvinhle <1559019@itec.hcmus.edu.vn>
2019-04-03 22:53:21 +0700

    class did not "end"
    Signed-off-by: khangvinhle <1559019@itec.hcmus.edu.vn>

--- app/workers/used_up_service_pack_job.rb
+++ app/workers/used_up_service_pack_job.rb
@@ -1,9 +1,13 @@
 class UsedUpServicePackJob < ApplicationJob
-	attr_reader :sp
-	def initialize(sp)
-		@sp = sp
-	end
-	def perform
-		User.where(admin: true).each do |user| ServicePacksMailer.used_up_email(user, @sp).deliver_now
-	end
+  attr_reader :sp
+
+  def initialize(sp)
+    @sp = sp
+  end
+
+  def perform
+    User.where(admin: true).each do |user|
+      ServicePacksMailer.used_up_email(user, @sp).deliver_now
+    end
+  end
 end
