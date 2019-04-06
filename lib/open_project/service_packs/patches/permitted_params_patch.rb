3af0d4f64c184eca62309b5cf29854973ff976b0
Le Cao Minh Tam <lcao.mtam@gmail.com>
2019-04-06 16:33:49 +0700

    Clean up patches

--- lib/open_project/service_packs/patches/permitted_params_patch.rb
+++ lib/open_project/service_packs/patches/permitted_params_patch.rb
@@ -5,7 +5,7 @@ module OpenProject::ServicePacks
 				permitted_params = params.fetch(:time_entry, {}).permit(
     				:hours, :comments, :work_package_id, :activity_id, :spent_on, :service_pack_id)
 				# binding.pry
-  			permitted_params.merge!(custom_field_values(:time_entry, required: false))
+  				permitted_params.merge!(custom_field_values(:time_entry, required: false))
 			end
 		end
 	end
