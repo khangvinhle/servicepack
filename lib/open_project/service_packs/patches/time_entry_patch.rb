8451267bf7f023e85109de6c736ea142aa72b2cb
Khang Le <1559019@itec.hcmus.edu.vn>
2019-04-11 22:07:08 +0700

    reformat code
    Signed-off-by: Le Vinh Khang <1559019@itec.hcmus.edu.vn>

--- lib/open_project/service_packs/patches/time_entry_patch.rb
+++ lib/open_project/service_packs/patches/time_entry_patch.rb
@@ -1,79 +1,79 @@
 module OpenProject::ServicePacks
-	module Patches
-		module TimeEntryPatch
-			module ClassMethods
+  module Patches
+    module TimeEntryPatch
+      module ClassMethods
 
-			end
+      end
 
 
-			# pseudocode:
-			# Find an assignment in effect, if not leave in peace.
-			# Then find a rate associated with activity_id and sp in effect.
-			# Create an SP_entry with the log entry cost.
-			# Subtract the remaining counter of SP to the cost.
+      # pseudocode:
+      # Find an assignment in effect, if not leave in peace.
+      # Then find a rate associated with activity_id and sp in effect.
+      # Create an SP_entry with the log entry cost.
+      # Subtract the remaining counter of SP to the cost.
 
-			module InstanceMethods
+      module InstanceMethods
 
-				def log_consumed_units
-					assignment = project.assigns.where(assigned: true).first
-					if assignment.nil?
+        def log_consumed_units
+          assignment = project.assigns.where(assigned: true).first
+          if assignment.nil?
             self.errors[:base] << "Cannot log time because none SP was assigned"
             raise ActiveRecord::Rollback
           end
-					activity_of_time_entry_id = self.activity.parent_id || self.activity.id
-					sp_of_project = assignment.service_pack
-					rate = sp_of_project.mapping_rates.find_by(activity_id: activity_of_time_entry_id).units_per_hour
-					units_cost = rate * self.hours
-					# binding.pry
-					sp_entry = ServicePackEntry.new(time_entry_id: id, units: units_cost)
-					sp_of_project.service_pack_entries << sp_entry
-					sp_of_project.update(remained_units: sp_of_project.remained_units - units_cost)
-				end
+          activity_of_time_entry_id = self.activity.parent_id || self.activity.id
+          sp_of_project = assignment.service_pack
+          rate = sp_of_project.mapping_rates.find_by(activity_id: activity_of_time_entry_id).units_per_hour
+          units_cost = rate * self.hours
+          # binding.pry
+          sp_entry = ServicePackEntry.new(time_entry_id: id, units: units_cost)
+          sp_of_project.service_pack_entries << sp_entry
+          sp_of_project.update(remained_units: sp_of_project.remained_units - units_cost)
+        end
 
 
-				def update_consumed_units
-					# First examine the entry
-					# then read the service pack related
-					# then recalculate the cost in the entry
-					# Update the entry
-					# Take the delta and subtract to the remained count of SP.
+        def update_consumed_units
+          # First examine the entry
+          # then read the service pack related
+          # then recalculate the cost in the entry
+          # Update the entry
+          # Take the delta and subtract to the remained count of SP.
 
-					sp_entry = self.service_pack_entry
-					return if sp_entry.nil?
-					sp_of_project = sp_entry.service_pack # the SP entry is binded at the point of creation
-					activity_of_time_entry_id = self.activity.parent_id || self.activity.id
-					rate = sp_of_project.mapping_rates.find_by(activity_id: activity_of_time_entry_id).units_per_hour
-					units_cost = rate * self.hours
+          sp_entry = self.service_pack_entry
+          return if sp_entry.nil?
+          sp_of_project = sp_entry.service_pack # the SP entry is binded at the point of creation
+          activity_of_time_entry_id = self.activity.parent_id || self.activity.id
+          rate = sp_of_project.mapping_rates.find_by(activity_id: activity_of_time_entry_id).units_per_hour
+          units_cost = rate * self.hours
 
-					extra_consumption = units_cost - sp_entry.units
-					# Keep callbacks for SP. Entries have no callback.
-					sp_entry.update(units: units_cost) if extra_consumption != 0
-					sp_of_project.remained_units -= extra_consumption
-					sp_of_project.save
-				end
+          extra_consumption = units_cost - sp_entry.units
+          # Keep callbacks for SP. Entries have no callback.
+          sp_entry.update(units: units_cost) if extra_consumption != 0
+          sp_of_project.remained_units -= extra_consumption
+          sp_of_project.save
+        end
 
-				def get_consumed_units_back
-					sp_entry = self.service_pack_entry
-					return if sp_entry.nil?
-					service_pack = sp_entry.service_pack
-					service_pack.remained_units += sp_entry.units
-					service_pack.save
-				end
+        def get_consumed_units_back
+          sp_entry = self.service_pack_entry
+          return if sp_entry.nil?
+          service_pack = sp_entry.service_pack
+          service_pack.remained_units += sp_entry.units
+          service_pack.save
+        end
 
-			end
+      end
 
-			def self.included(receiver)
-				receiver.extend         ClassMethods
-				receiver.send :include, InstanceMethods
-				receiver.class_eval do
-					has_one :service_pack_entry, dependent: :destroy
-					belongs_to :service_pack
-					after_create :log_consumed_units
-					after_update :update_consumed_units
-					before_destroy :get_consumed_units_back
-				end
-			end
+      def self.included(receiver)
+        receiver.extend ClassMethods
+        receiver.send :include, InstanceMethods
+        receiver.class_eval do
+          has_one :service_pack_entry, dependent: :destroy
+          belongs_to :service_pack
+          after_create :log_consumed_units
+          after_update :update_consumed_units
+          before_destroy :get_consumed_units_back
+        end
+      end
 
-		end
-	end
+    end
+  end
 end
