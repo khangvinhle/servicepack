b18e7b0662985c716fcc9237c24d8113e6b26ca7
Le Cao Minh Tam <lcao.mtam@gmail.com>
2019-05-07 20:30:04 +0700

    Fix broken sufficient units check

--- lib/open_project/service_packs/patches/update_contract_patch.rb
+++ lib/open_project/service_packs/patches/update_contract_patch.rb
@@ -6,7 +6,7 @@ module OpenProject::ServicePacks
           unless model.project.assigns.active.find_by(service_pack_id: service_pack_id)
             errors.add :service_pack_id, -'This Service Pack is not assigned to this project'
           else
-            if ServicePack.find(service_pack_id).remained_units < model.calculate_unit_costs
+            if ServicePack.find(service_pack_id).remained_units < model.calculate_units_cost
               errors.add :base, -'Service Pack selected does not have enough units!'
             end
           end
