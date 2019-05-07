8db3d3ec350510192b75917049f1c71d5155e8a7
Le Cao Minh Tam <lcao.mtam@gmail.com>
2019-05-07 21:39:24 +0700

    Add ETag to assignments API

--- lib/api/v3/service_packs/assignments_api.rb
+++ lib/api/v3/service_packs/assignments_api.rb
@@ -14,13 +14,24 @@ module API
                              unassign_service_packs), projects: @project) # 4 first permissions are for time logging
           end
 
+          helpers do
+            def lite_etag
+              key = @assignments.map {|a| "#{a.id}!#{a.service_pack_id}!#{a.updated_at}"}.join(-'|')
+              hash_to_compare = Digest::SHA1.hexdigest(key)
+              error!(-'Not Modified', 304) if request.headers[-'If-None-Match'] == hash_to_compare
+              header -'ETag', hash_to_compare
+            end
+          end
+
           get do
             unless @project.enabled_modules.find_by(name: -'service_packs')
               raise ::API::Errors::ErrorsBase.new(message: -'Service Packs module is disabled for this project')
             end
             
             @assignments = @project.assigns.active.preload(:service_pack)
-            raise ::API::Errors::NotFound.new(message: -'No active assignments found') if @assignments.empty?
+            # raise ::API::Errors::NotFound.new(message: -'No Service Packs are assigned to this project') if @assignments.empty?
+            lite_etag
+            # just an empty array should be enough
             ::API::V3::ServicePacks::AssignmentCollectionRepresenter.new(@assignments, @assignments.length, -'',
                                                                          current_user: current_user)
           end
